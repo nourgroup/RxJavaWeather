c3dfed68236083b050fb55e3722cadabc60817b9
oummadi <n.oummadi.e@gmail.com>
2022-11-05 14:20:32 +0100

    observer-publishSubject

--- app/src/main/java/com/ngplus/rxjava/MainActivity.java
+++ app/src/main/java/com/ngplus/rxjava/MainActivity.java
@@ -3,6 +3,7 @@ package com.ngplus.rxjava;
 import androidx.appcompat.app.AppCompatActivity;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.observables.ConnectableObservable;
+import io.reactivex.rxjava3.subjects.PublishSubject;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Observable<Long> test = Observable.intervalRange(0,5,0,1, TimeUnit.SECONDS);
-        ConnectableObservable<Long> cold = ConnectableObservable.intervalRange(0,5,0,1, TimeUnit.SECONDS).publish();
+        /*ConnectableObservable<Long> cold = ConnectableObservable.intervalRange(0,5,0,1, TimeUnit.SECONDS).publish();
         cold.connect();
         cold.subscribe(i -> Log.i("MainActivity_rxjava","student 1 -> "+i.toString()));
 
@@ -24,7 +25,32 @@ public class MainActivity extends AppCompatActivity {
             Thread.sleep(2000);
         }catch(Exception ex){
         }
-        cold.subscribe(i -> Log.i("MainActivity_rxjava","Student 2 -> "+i.toString()));
+        cold.subscribe(i -> Log.i("MainActivity_rxjava","Student 2 -> "+i.toString()));*/
+        /***Observable : PublishSubject ****/
+        PublishSubject<String> mySubject = PublishSubject.create();
+        mySubject.subscribe(i -> Log.i("MainActivity_rxjava","student 1 -> "+i));
 
+        mySubject.onNext("A");
+        sleep(1000);
+        mySubject.onNext("B");
+        sleep(1000);
+        mySubject.onNext("C");
+        sleep(1000);
+        mySubject.onNext("D");
+        sleep(1000);
+        mySubject.subscribe(i -> Log.i("MainActivity_rxjava","student 2 -> "+i));
+        mySubject.onNext("E");
+        sleep(1000);
+        mySubject.onNext("F");
+        sleep(1000);
+
+    }
+
+    void sleep(int ms){
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
